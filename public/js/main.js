32412147fdc6fcbf4ded40cdd7508a2ff020b2bf
Jan S. <59015445+janstricker@users.noreply.github.com>
2025-07-10 19:17:47 +0000

    removed hamburger menu

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,2 @@
 (() => {
-  // <stdin>
-  document.addEventListener("DOMContentLoaded", () => {
-    const hamburger = document.querySelector(".hamburger");
-    const sidebar = document.querySelector(".sidebar");
-    hamburger.addEventListener("click", () => {
-      hamburger.classList.toggle("active");
-      sidebar.classList.toggle("active");
-    });
-  });
 })();
--- themes/futheme/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.querySelector('.hamburger');
-    const sidebar = document.querySelector('.sidebar');
-    
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('active');
-        sidebar.classList.toggle('active');
-    });
-});
